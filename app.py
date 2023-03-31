02d69676a3034cfe8d84c3e8212addb0652d9b43
Ezequiel Abregu <ezequielabregu.code@gmail.com>
2023-03-31 10:21:51 +0200

    Fix relative paths

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         f = request.files['audio_data']
-        with open('audio.wav', 'wb') as audio:
+        with open('./audio.wav', 'wb') as audio:
             f.save(audio)
         print('file uploaded successfully')
 
@@ -23,7 +23,7 @@ def upload():
     #requiest the file name
     f = request.files['file']
     #save the audio file
-    f.save("audio.wav")
+    f.save("./audio.wav")
     print ('Audio file uploaded')
     return render_template('index.html', uploadedName=f.filename)
     #return redirect(url_for('transcribe'))  
@@ -31,7 +31,7 @@ def upload():
 
 @app.route('/download')
 def download_file():
-    path = "static/output.txt"   
+    path = "./static/output.txt"   
     return send_file(path, as_attachment=True)
 
 @app.route('/transcribe', methods=['POST'])
@@ -39,25 +39,25 @@ def transcribe():
     #create a new recognizer instance
     r = sr.Recognizer()
     #use the audio uploaded as a source
-    with sr.AudioFile('audio.wav') as source:
+    with sr.AudioFile('./audio.wav') as source:
         audio = r.record(source)
         try:    
             #audio to text    
             text = r.recognize_google(audio)
        # write the text to a file
-            with open("static/output.txt", "w") as file:
+            with open("./static/output.txt", "w") as file:
                 file.write(text)
-                os.remove('audio.wav')            
+                os.remove('./audio.wav')            
             return render_template('index.html', text=text)
         
         except sr.UnknownValueError:
             text="Could not understand you. Try Again!"
-            os.remove('audio.wav')  
+            os.remove('./audio.wav')  
             return render_template('index.html', text=text)
 
         except sr.RequestError as e:
             text = "Could not request results from Google Speech Recognition service; {0}".format(e)    
-            os.remove('audio.wav')  
+            os.remove('./audio.wav')  
             return render_template('index.html', text=text)
     
 if __name__ == '__main__':
