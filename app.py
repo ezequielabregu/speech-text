d41873bf204fa9204ca45b7fe347e3cc2b427c7e
Ezequiel Abregu <ezequielabregu.code@gmail.com>
2023-04-04 10:40:24 +0200

    Update app.py to the initial deploy on a VPS subdomain

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ import os
 #Crete a new instance Flask class
 app = Flask(__name__)
 
-server_path = "/var/www/html/flask_app/"
+server_path = "" #"/var/www/html/flask_app/"
 audio_path = server_path + "audio.wav"
 text_path = server_path + "output.txt"
-
-print (text_path)
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
